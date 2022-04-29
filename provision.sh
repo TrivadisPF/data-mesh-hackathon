1e3d4a3af96d3ccf2471f4a094f49e237331d934
Guido Schmutz <guido.schmutz@trivadis.com>
2022-04-29 19:13:37 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -21,10 +21,9 @@ mkdir -p $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/customer
 cp -r ./simulator/customer/data/* $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/customer
 
 # copy order data
-mkdir -p $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/order
-unzip ./simulator/order/data.zip
-mv .csv $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/order
-rm -R __MACOSX/
+mkdir -p $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator
+unzip ./simulator/order/data.zip -d $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator
+mv $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/data $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/order
 
 # copy docker-compose.override file
 cp  ./source/infra/docker-compose.override.yml $DATAPLATFORM_HOME
