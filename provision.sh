49d5e0aa079a0c7c852f399494d6dc4a86393545
Guido Schmutz <guido.schmutz@trivadis.com>
2022-04-29 19:19:24 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -21,7 +21,7 @@ mkdir -p $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/customer
 cp -r ./simulator/customer/data/* $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/customer
 
 # copy order data
-rm $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/order
+rm -R $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/order
 mkdir -p $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator
 unzip ./simulator/order/data.zip -d $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator
 mv $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/data $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/order
