71fa8e4f5810ec52d2c6d62dbd256ea2db9aeefc
Guido Schmutz <guido.schmutz@trivadis.com>
2022-05-09 21:24:24 +0200

    update

---
+++ implementation/customer/oracle/01_create-user.sh
@@ -0,0 +1,20 @@
+-- ----------------------------------------------
+-- User ecomm_customer (legacy schema)
+-- ----------------------------------------------
+
+./createAppUser ecomm_customer abc123!
+
+-- ----------------------------------------------
+-- User ecomm_customer_priv
+-- ----------------------------------------------
+
+./createAppUser ecomm_customer_priv abc123!
+
+-- ----------------------------------------------
+-- User ecomm_customer_pub_v1
+-- ----------------------------------------------
+
+./createAppUser ecomm_customer_pub_v1 abc123!
+
+    
+    
