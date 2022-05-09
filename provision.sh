6caa13910faf16e00b80eb2418a435a0fce13b40
Guido Schmutz <guido.schmutz@trivadis.com>
2022-05-09 21:18:35 +0200

    update

--- provision.sh
+++ provision.sh
@@ -12,22 +12,29 @@
 #platys gen
 
 
+# copy Oracle DB scripts for Customer domain
+cp -r ./implementation/customer/oracle/*.sql $DATAPLATFORM_HOME/init/oraclexe
+
+# copy Postgresql scripts for Customer domain
+cp -r ./implementation/customer/oracle/*.sql $DATAPLATFORM_HOME/init/oraclexe
+
+
 # copy streamsets properties file
-cp -r ./source/infra/streamsets/configuration.properties $DATAPLATFORM_HOME/custom-conf/streamsets
+cp -r ./implementation/infra/streamsets/configuration.properties $DATAPLATFORM_HOME/custom-conf/streamsets
 
 
 # copy customer data
 mkdir -p $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/customer
 cp -r ./simulator/customer/data/* $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/customer
 
-# copy order data
-rm -R $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/order
+# copy salesorder data
+rm -R $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/salesorder
 mkdir -p $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator
-unzip ./simulator/order/data.zip -d $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator
-mv $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/data $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/order
+unzip ./simulator/salesorder/salesorder-data.zip -d $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator
+mv $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/data $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/salesorder
 rm -R $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/__MACOSX/
 
 # copy docker-compose.override file
-cp  ./source/infra/docker-compose.override.yml $DATAPLATFORM_HOME
+cp  ./implementation/infra/docker-compose.override.yml $DATAPLATFORM_HOME
 
 
