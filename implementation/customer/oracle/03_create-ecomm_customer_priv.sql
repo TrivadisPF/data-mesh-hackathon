9e0024d5e384bdbe22d5582e6c46198330651b26
Guido Schmutz <guido.schmutz@trivadis.com>
2022-05-09 22:20:57 +0200

    Update 03_create-ecomm_customer_priv.sql

--- implementation/customer/oracle/03_create-ecomm_customer_priv.sql
+++ implementation/customer/oracle/03_create-ecomm_customer_priv.sql
@@ -34,26 +34,4 @@ GRANT SELECT ON person_phone_v TO ecomm_customer_pub_v1;
 GRANT SELECT ON email_address_v TO ecomm_customer_pub_v1;
 
 
-CREATE OR REPLACE VIEW ecomm_customer_priv.customer_sales_v
-as 
-select c.customerid
-,      sum(nvl(amount,0)) as salesvolume
-from (
-    select jt.orderdate, sum (to_number(unitprice default 1 on conversion error)*nvl(quantity,0)) amount
-    ,      trunc(dbms_random.value(1,19000)) customerid
-    from "CUSTOMER"."CUSTOMER_ORDERS" t
-    ,    json_table (message, '$'
-             COLUMNS (
-                  orderdate path '$.order.orderDate'
-                , NESTED PATH '$.order.items[*]'
-                  COLUMNS (
-                          unitprice path '$.unitPrice'
-                        , quantity path '$.quantity' )
-                )
-                ) jt 
-    group by   orderdate    
-    ) o
-    right join v_customer c on c.customerid = o.customerid
-group by c.customerid
-order by 2 desc;
 
