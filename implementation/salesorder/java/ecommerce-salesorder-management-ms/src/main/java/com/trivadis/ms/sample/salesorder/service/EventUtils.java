da4daefe083ab84f2f574ddcaae25b964386c284
Guido Schmutz <guido.schmutz@trivadis.com>
2022-05-11 22:42:31 +0200

    update

--- implementation/salesorder/java/ecommerce-salesorder-management-ms/src/main/java/com/trivadis/ms/sample/salesorder/service/EventUtils.java
+++ implementation/salesorder/java/ecommerce-salesorder-management-ms/src/main/java/com/trivadis/ms/sample/salesorder/service/EventUtils.java
@@ -3,16 +3,20 @@ package com.trivadis.ms.sample.salesorder.service;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.trivadis.ecommerce.salesorder.priv.avro.SalesOrder;
+import com.trivadis.ecommerce.salesorder.priv.avro.SalesOrderAcceptedEvent;
 import com.trivadis.ms.sample.salesorder.converter.SalesOrderConverter;
 import com.trivadis.ms.sample.salesorder.model.SalesOrderDO;
 import com.trivadis.ms.sample.salesorder.outbox.model.OutboxEvent;
 import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
 import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Utility class to help the service in building event payloads.
@@ -45,15 +49,20 @@ public class EventUtils {
     private OutboxEvent createSalesOrderSubmitEventAvro(SalesOrderDO salesOrderDO) {
 
         SchemaRegistryClient schemaRegistryClient = new CachedSchemaRegistryClient(schemaRegistryUrl, 10);
-        KafkaAvroSerializer ser = new KafkaAvroSerializer(schemaRegistryClient);
+        Map<String, Object> props = new HashMap<>();
+        // send correct schemas to the registry, without "avro.java.string"
+        props.put(KafkaAvroSerializerConfig.AVRO_REMOVE_JAVA_PROPS_CONFIG, true);
+        props.put("schema.registry.url", schemaRegistryUrl);
+        KafkaAvroSerializer ser = new KafkaAvroSerializer(schemaRegistryClient, props);
 
         SalesOrder salesOrder = SalesOrderConverter.convertToAvro(salesOrderDO);
+        SalesOrderAcceptedEvent salesOrderAcceptedEvent = SalesOrderAcceptedEvent.newBuilder().setSalesOrder(salesOrder).build();
 
         return new OutboxEvent(
                 salesOrderDO.getId(),
                 "order-accepted",
                 null,
-                ser.serialize("priv.ecomm.salesorder.order-accepted.event.v1", salesOrder)
+                ser.serialize("priv.ecomm.salesorder.order-accepted.event.v1", salesOrderAcceptedEvent)
         );
     }
 
