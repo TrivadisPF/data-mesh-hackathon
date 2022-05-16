02ef3fd382d7ac332e5ca82396d8714bf6f7358d
Guido Schmutz <guido.schmutz@trivadis.com>
2022-05-16 22:52:08 +0200

    add country_code_t table to ecomm_customer_priv schema

--- implementation/customer/oracle/03_create-ecomm_customer_priv.sql
+++ implementation/customer/oracle/03_create-ecomm_customer_priv.sql
@@ -46,4 +46,29 @@ GRANT SELECT ON phone_number_type_v TO ecomm_customer_pub_v1;
 GRANT SELECT ON state_province_v TO ecomm_customer_pub_v1;
 
 
+DROP TABLE country_code_t;
+
+CREATE TABLE country_code_t (
+    name		  				VARCHAR2(100) PRIMARY KEY,
+    alpha_2  					VARCHAR2(2),
+    alpha_3  					VARCHAR2(3),
+    country_code	  			NUMBER(10),
+    iso3166_2	  				VARCHAR2(20),
+    region						VARCHAR2(50),
+    sub_region					VARCHAR2(50),
+    intermediate_region			VARCHAR2(50),
+    region_code					NUMBER(10),
+    sub_region_code				NUMBER(10),
+    intermediate_region_code	NUMBER(10),
+    created_date		    	TIMESTAMP,
+    modified_date    			TIMESTAMP);
+    
+CREATE OR REPLACE VIEW country_code_v
+AS
+SELECT *
+FROM country_code_t;
+
+GRANT SELECT ON country_code_v TO ecomm_customer_pub_v1;
+
+
 
