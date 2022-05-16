a50094b9043e8e4672def2ed5d79c3595e4fea39
Guido Schmutz <guido.schmutz@trivadis.com>
2022-05-16 12:40:16 +0200

    update

--- implementation/salesorder/kafka-connect/start-connect-jdbc.sh
+++ implementation/salesorder/kafka-connect/start-connect-jdbc.sh
@@ -6,7 +6,7 @@ curl -X "DELETE" "http://$DATAPLATFORM_IP:8083/connectors/salesorder.dbzsrc.outb
 
 echo "Creating Debezium Source Connector on SalesOrder.outbox"
 
-## Request
+## Request for using payload as JSON
 curl -X PUT \
   "http://$DATAPLATFORM_IP:8083/connectors/salesorder.dbzsrc.outbox/config" \
   -H 'Content-Type: application/json' \
@@ -29,8 +29,39 @@ curl -X PUT \
   "transforms.outbox.type": "io.debezium.transforms.outbox.EventRouter",
   "transforms.outbox.table.field.event.id": "id",
   "transforms.outbox.table.field.event.key": "aggregate_id",
-  "transforms.outbox.table.field.event.payload": "payload",
+  "transforms.outbox.table.field.event.payload": "payload_json",
   "transforms.outbox.table.field.event.timestamp": "created_at",
   "transforms.outbox.route.by.field": "event_type",
-  "transforms.outbox.route.topic.replacement": "priv.${routedByValue}.event.v1"
+  "transforms.outbox.route.topic.replacement": "priv.ecomm.salesorder.${routedByValue}.event.v1"
+}'
+
+
+## Request for using payload as Avro
+curl -X PUT \
+  "http://$DATAPLATFORM_IP:8083/connectors/salesorder.dbzsrc.outbox/config" \
+  -H 'Content-Type: application/json' \
+  -H 'Accept: application/json' \
+  -d '{
+  "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+  "tasks.max": "1",
+
+  "database.server.name": "postgresql",
+  "database.port": "5432",
+  "database.user": "postgres",
+  "database.password": "abc123!",  
+  "database.dbname": "postgres",
+  "schema.include.list": "salesorder",
+  "table.include.list": "salesorder.outbox",
+  "plugin.name": "pgoutput",
+  "tombstones.on.delete": "false",
+  "database.hostname": "postgresql",
+  "transforms": "outbox",  
+  "transforms.outbox.type": "io.debezium.transforms.outbox.EventRouter",
+  "transforms.outbox.table.field.event.id": "id",
+  "transforms.outbox.table.field.event.key": "aggregate_id",
+  "transforms.outbox.table.field.event.payload": "payload_avro",
+  "transforms.outbox.table.field.event.timestamp": "created_at",
+  "transforms.outbox.route.by.field": "event_type",
+  "transforms.outbox.route.topic.replacement": "priv.ecomm.salesorder.${routedByValue}.event.v1",
+  "value.converter": "io.debezium.converters.ByteBufferConverter"
 }'
