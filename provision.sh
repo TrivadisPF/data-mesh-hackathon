23f9dd597de4a5043c7ee428a15a4e64afd06b1e
Guido Schmutz <guido.schmutz@trivadis.com>
2022-05-28 15:01:50 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -5,29 +5,29 @@ cp -rv ./implementation/customer/oracle/*.sh $DATAPLATFORM_HOME/init/oraclexe
 cp -rv ./implementation/customer/oracle/*.sql $DATAPLATFORM_HOME/init/oraclexe
 
 echo "copy Postgresql scripts for Sales Order domain"
-cp -r ./implementation/salesorder/postgresql/*.sh $DATAPLATFORM_HOME/init/postgresql
-cp -r ./implementation/salesorder/postgresql/*.sql $DATAPLATFORM_HOME/init/postgresql
+#cp -rv ./implementation/salesorder/postgresql/*.sh $DATAPLATFORM_HOME/init/postgresql
+cp -rv ./implementation/salesorder/postgresql/*.sql $DATAPLATFORM_HOME/init/postgresql
 
 
-# copy streamsets properties file
-cp -r ./implementation/infra/streamsets/configuration.properties $DATAPLATFORM_HOME/custom-conf/streamsets
+echo "copy streamsets properties file"
+cp -rv ./implementation/infra/streamsets/configuration.properties $DATAPLATFORM_HOME/custom-conf/streamsets
 
-# copy customer data
+echo "copy customer data"
 mkdir -p $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/ref
-cp -r ./simulator/ref/data/* $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/ref
+cp -rv ./simulator/ref/data/* $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/ref
 
-# copy customer data
+echo "copy customer data"
 mkdir -p $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/customer
-cp -r ./simulator/customer/data/* $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/customer
+cp -rv ./simulator/customer/data/* $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/customer
 
-# copy salesorder data
+echo "copy salesorder data"
 rm -fR $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/salesorder
 mkdir -p $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator
 unzip ./simulator/salesorder/salesorder-data.zip -d $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator
 mv $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/data $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/salesorder
 rm -fR $DATAPLATFORM_HOME/data-transfer/data-mesh-poc/simulator/__MACOSX/
 
-# copy docker-compose.override file
-cp  ./implementation/infra/docker-compose.override.yml $DATAPLATFORM_HOME
+echo "copy docker-compose.override file"
+cp -v ./implementation/infra/docker-compose.override.yml $DATAPLATFORM_HOME
 
 
