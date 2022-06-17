0c896f488eb28ec8cc344a4c579a265fa61b35a3
Guido Schmutz <guido.schmutz@trivadis.com>
2022-06-17 07:52:50 +0200

    Update create-hive-tables.sh

--- implementation/scripts/create-hive-tables.sh
+++ implementation/scripts/create-hive-tables.sh
@@ -9,11 +9,11 @@ fi
 echo -e "\n--\n+> Creating Hive Tables"
 
 docker cp $1/implementation/salesorder/hive/hive-salesorder.sql hive-metastore:/tmp
-docker exec -ti hive-metastore hive -f /tmp/hive-salesoder.sql
+docker exec -ti hive-metastore hive -f /tmp/hive-salesoder.sql -v
 
 docker cp $1/implementation/customer/hive/hive-customer.sql hive-metastore:/tmp
-docker exec -ti hive-metastore hive -f /tmp/hive-customer.sql
+docker exec -ti hive-metastore hive -f /tmp/hive-customer.sql -v
 
 docker cp $1/implementation/product/hive/hive-product.sql hive-metastore:/tmp
-docker exec -ti hive-metastore hive -f /tmp/hive-product.sql
+docker exec -ti hive-metastore hive -f /tmp/hive-product.sql -v
 
