58399ad49d3fb9d624d8dd5ed2c6948e66bdb2c4
Guido Schmutz <guido.schmutz@trivadis.com>
2022-06-19 08:52:10 +0200

    Update trino-product.sql

--- implementation/product/trino/trino-product.sql
+++ implementation/product/trino/trino-product.sql
@@ -2,24 +2,20 @@ USE pub_ecomm_product;
 
 CREATE OR REPLACE VIEW product_aggr_v
 AS
-SELECT	product.id 			AS id
-, 		name 				AS name
-,       productNumber		AS product_number
-,       makeFlag			AS make_flag
-,		finishedGoodsFlag 	AS finished_goods_flag
-,		color				AS color
-,		safetyStockLevel	AS safety_stock_level
-,		reorderPoint		AS reorder_point
-,		standardCost		AS standard_cost
-,		listPrice			AS list_price
-,		size				AS size
-,		weight				AS weight
-,		daysToManufacture	AS days_to_manufacture
-,		productLine			AS product_line
-,		class				AS class
-,		style				AS style
-FROM product_state_t;
-
-
-
-
+SELECT	prod.product.id 				AS id
+, 		prod.product.name 				AS name
+,       prod.product.productNumber		AS product_number
+,       prod.product.makeFlag			AS make_flag
+,		prod.product.finishedGoodsFlag 	AS finished_goods_flag
+,		prod.product.color				AS color
+,		prod.product.safetyStockLevel	AS safety_stock_level
+,		prod.product.reorderPoint		AS reorder_point
+,		prod.product.standardCost		AS standard_cost
+,		prod.product.listPrice			AS list_price
+,		prod.product.size				AS size
+,		prod.product.weight				AS weight
+,		prod.product.daysToManufacture	AS days_to_manufacture
+,		prod.product.productLine		AS product_line
+,		prod.product.class				AS class
+,		prod.product.style				AS style
+FROM product_state_t	prod;
