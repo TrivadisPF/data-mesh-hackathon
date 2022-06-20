da39e1580ccae7e3c6b9c3ddead7751efbe62be8
pwelker <45032324+pwelker@users.noreply.github.com>
2022-06-20 16:11:20 +0200

    Adaptions for shop events and ksql
    - added shopevent data
    - corrected ksql provisioning
    - enhances online order simulator incl. shop events

--- implementation/scripts/create-ksql-objects.sh
+++ implementation/scripts/create-ksql-objects.sh
@@ -8,12 +8,12 @@ fi
 
 echo -e "\n--\n+> Creating KSQL objects"
 
-docker cp $1/implementation/customer/ksql/customer-v1.ksql ksqldb-server-1:/tmp
-docker exec -it ksqldb-cli ksql http://ksqldb-server-1:8088 -f /tmp/customer-v1.sql
+docker cp $1/implementation/customer/ksql/customer-v1.ksql ksqldb-cli:/tmp
+docker exec -it ksqldb-cli ksql http://ksqldb-server-1:8088 -f /tmp/customer-v1.ksql
 
-docker cp $1/implementation/customer/ksql/prodocut-v1.ksql ksqldb-server-1:/tmp
-docker exec -it ksqldb-cli ksql http://ksqldb-server-1:8088 -f /tmp/product-v1.sql
+docker cp $1/implementation/product/ksql/product-v1.ksql ksqldb-cli:/tmp
+docker exec -it ksqldb-cli ksql http://ksqldb-server-1:8088 -f /tmp/product-v1.ksql
 
-docker cp $1/implementation/salesorder/ksql/salesorder-v1.ksql ksqldb-server-1:/tmp
-docker exec -it ksqldb-cli ksql http://ksqldb-server-1:8088 -f /tmp/salesorder-v1.sql
+docker cp $1/implementation/salesorder/ksql/salesorder-v1.ksql ksqldb-cli:/tmp
+docker exec -it ksqldb-cli ksql http://ksqldb-server-1:8088 -f /tmp/salesorder-v1.ksql
 
