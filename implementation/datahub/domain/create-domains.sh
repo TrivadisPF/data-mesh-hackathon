292528238d2e7ac5ddfaff112d81a48186c81e70
Guido Schmutz <guido.schmutz@trivadis.com>
2023-11-25 21:06:37 +0100

    update

--- implementation/datahub/domain/create-domains.sh
+++ implementation/datahub/domain/create-domains.sh
@@ -6,33 +6,33 @@ curl --location --request POST ${DATAHUB_GMS_URL}/api/graphql \
 
 curl --location --request POST ${DATAHUB_GMS_URL}/api/graphql \
 --header 'Content-Type: application/json' \
---data-raw '{ "query": "mutation createDomain { createDomain(input: { id: \"shop\", name: \"Shop\", description: \"Entities related to the shop department.\" }) }", "variables":{}}'
+--data-raw '{ "query": "mutation createDomain { createDomain(input: { id: \"shop\", parentDomain: \"ecommerce\", name: \"Shop\", description: \"Entities related to the shop department.\" }) }", "variables":{}}'
 
 curl --location --request POST ${DATAHUB_GMS_URL}/api/graphql \
 --header 'Content-Type: application/json' \
---data-raw '{ "query": "mutation createDomain { createDomain(input: { id: \"sasleorder\", name: \"SaleOrder\", description: \"Entities related to the sales order department.\" }) }", "variables":{}}'
+--data-raw '{ "query": "mutation createDomain { createDomain(input: { id: \"sasleorder\", parentDomain: \"ecommerce\", name: \"SaleOrder\", description: \"Entities related to the sales order department.\" }) }", "variables":{}}'
 
 curl --location --request POST ${DATAHUB_GMS_URL}/api/graphql \
 --header 'Content-Type: application/json' \
---data-raw '{ "query": "mutation createDomain { createDomain(input: { id: \"customer\", name: \"Customer\", description: \"Entities related to the customer department.\" }) }", "variables":{}}'
+--data-raw '{ "query": "mutation createDomain { createDomain(input: { id: \"customer\", parentDomain: \"ecommerce\", name: \"Customer\", description: \"Entities related to the customer department.\" }) }", "variables":{}}'
 
 curl --location --request POST ${DATAHUB_GMS_URL}/api/graphql \
 --header 'Content-Type: application/json' \
---data-raw '{ "query": "mutation createDomain { createDomain(input: { id: \"product\", name: \"Product\", description: \"Entities related to the Product domain.\" }) }", "variables":{}}'
+--data-raw '{ "query": "mutation createDomain { createDomain(input: { id: \"product\", parentDomain: \"ecommerce\", name: \"Product\", description: \"Entities related to the Product domain.\" }) }", "variables":{}}'
 
 curl --location --request POST ${DATAHUB_GMS_URL}/api/graphql \
 --header 'Content-Type: application/json' \
---data-raw '{ "query": "mutation createDomain { createDomain(input: { id: \"customerinteraction\", name: \"Customer Interaction\", description: \"Entities related to the customer department.\" }) }", "variables":{}}'
+--data-raw '{ "query": "mutation createDomain { createDomain(input: { id: \"customerinteraction\", parentDomain: \"ecommerce\", name: \"Customer Interaction\", description: \"Entities related to the customer department.\" }) }", "variables":{}}'
 
 curl --location --request POST ${DATAHUB_GMS_URL}/api/graphql \
 --header 'Content-Type: application/json' \
---data-raw '{ "query": "mutation createDomain { createDomain(input: { id: \"curnDetection\", name: \"Churn Detection\", description: \"Entities related to the customer department.\" }) }", "variables":{}}'
+--data-raw '{ "query": "mutation createDomain { createDomain(input: { id: \"curnDetection\", parentDomain: \"ecommerce\", name: \"Churn Detection\", description: \"Entities related to the customer department.\" }) }", "variables":{}}'
 
 curl --location --request POST ${DATAHUB_GMS_URL}/api/graphql \
 --header 'Content-Type: application/json' \
---data-raw '{ "query": "mutation createDomain { createDomain(input: { id: \"recommendation\", name: \"Recommediation\", description: \"Entities related to the customer department.\" }) }", "variables":{}}'
+--data-raw '{ "query": "mutation createDomain { createDomain(input: { id: \"recommendation\", parentDomain: \"ecommerce\", name: \"Recommediation\", description: \"Entities related to the customer department.\" }) }", "variables":{}}'
 
 curl --location --request POST ${DATAHUB_GMS_URL}/api/graphql \
 --header 'Content-Type: application/json' \
---data-raw '{ "query": "mutation createDomain { createDomain(input: { id: \"reference\", name: \"Reference\", description: \"Entities related to the reference department.\" }) }", "variables":{}}'
+--data-raw '{ "query": "mutation createDomain { createDomain(input: { id: \"reference\", parentDomain: \"ecommerce\", name: \"Reference\", description: \"Entities related to the reference department.\" }) }", "variables":{}}'
 
